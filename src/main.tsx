0fe60a1e7917266ec428378c734ddb331c5094fb
gyeongza <gyeongza@naver.com>
2023-12-01 20:06:14 +0900

    feat: Alert Context Api 구현

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { Global } from '@emotion/react';
 import globalStyles from './styles/globalStyles.ts';
+import { AlertContextProvider } from './contexts/AlertContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Global styles={globalStyles} />
-    <App />
+    <AlertContextProvider>
+      <App />
+    </AlertContextProvider>
   </React.StrictMode>,
 );
