001f50947cb94eb295e4223cbc190e9995d2f7d1
gyeongza <gyeongza@naver.com>
2023-12-01 18:38:54 +0900

    feat: emotion 설정 및 Text, Button 공용 컴포넌트 구현

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      // JSX 컴파일 시 기본 jsx-runtime 대신 Emotion의 jsx 함수를 사용하도록 설정
+      // JSX에서 css prop을 사용하기 위함
+      jsxImportSource: '@emotion/react',
+      // 커스텀 바벨 설정: @emotion-babel-plugin 플러그인 추가
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
