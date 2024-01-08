a23d22d731f653e9591c76adce2735520e629255
gyeongza <gyeongza@naver.com>
2024-01-08 18:50:40 +0900

    feat: ScrollToTop 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Test from './pages/Test';
 import Card from './pages/Card';
+import ScrollToTop from './components/common/ScrollToTop';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/card/:id" Component={Card} />
