9903f0e7a0b6ff59ee4b41785d616f72a96158d5
gyeongza <gyeongza@naver.com>
2024-01-17 01:24:42 +0900

    refactor: 카드 신청 페이지 컴포넌트 관심사 분리

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -1,35 +1,9 @@
-import BasicInfo from '@/components/apply/BasicInfo';
-import CardInfo from '@/components/apply/CardInfo';
-import Terms from '@/components/apply/Terms';
-import { ApplyValues } from '@/models/apply';
-import { useState } from 'react';
+import ApplyInfo from '@/components/apply';
 
 function Apply() {
-  const [step, setStep] = useState(2);
+  const handleSubmit = () => {};
 
-  const handleTermsChange = (terms: ApplyValues['terms']) => {
-    console.log(terms);
-  };
-
-  const handleBasicInfoChange = (
-    infoValues: Pick<ApplyValues, 'salary' | 'creditScore' | 'payDate'>,
-  ) => {
-    console.log(infoValues);
-  };
-
-  const handleCardInfoChange = (
-    cardInfoValues: Pick<ApplyValues, 'isHipass' | 'isMaster' | 'isRf'>,
-  ) => {
-    console.log(cardInfoValues);
-  };
-
-  return (
-    <div>
-      {step === 0 ? <Terms onNext={handleTermsChange} /> : null}
-      {step === 1 ? <BasicInfo onNext={handleBasicInfoChange} /> : null}
-      {step === 2 ? <CardInfo onNext={handleCardInfoChange} /> : null}
-    </div>
-  );
+  return <ApplyInfo onSubmit={handleSubmit} />;
 }
 
 export default Apply;
